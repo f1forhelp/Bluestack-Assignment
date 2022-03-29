e270a459a41118853447853599d9d4fefa0e282a
utkarshsharma1111 <84666642+utkarshsharma1111@users.noreply.github.com>
2022-03-29 15:11:08 +0530

    Some minor chnages

--- lib/features/Authentication/presentation/cubit/authentication_state.dart
+++ lib/features/Authentication/presentation/cubit/authentication_state.dart
@@ -1,10 +1,8 @@
 part of 'authentication_cubit.dart';
 
-abstract class AuthenticationState extends Equatable {
+class AuthenticationState {
   const AuthenticationState();
 
   @override
   List<Object> get props => [];
 }
-
-class AuthenticationInitial extends AuthenticationState {}
